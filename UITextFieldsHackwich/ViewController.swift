c00b4a997a161ecee64de59d3907d8eb5384766c
Aiden Martinez <Angrybull2007@gmail.com>
2022-02-09 14:13:31 -0700

    ButtonThing

--- UITextFieldsHackwich/ViewController.swift
+++ UITextFieldsHackwich/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+// Corner Radius Extension For The Button
+@IBDesignable extension UIButton {
 
-class ViewController: UIViewController {
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
 
-    override func viewDidLoad() {
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+class ViewController: UIViewController
+{
+    @IBOutlet weak var TextIn: UITextField!
+    @IBOutlet weak var LabelDisplay: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func UsingButton(_ sender: Any)
+    {
+        
+        LabelDisplay.text = TextIn.text
+        
+    }
+    
 }
 
